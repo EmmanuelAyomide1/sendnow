6bce198bc3dfd3ec76cea2c009ceddc941e87a46
Owoeye Emmanuel Ayomide <emmanuel@Owoeyes-MacBook-Pro.local>
2025-10-03 15:37:51 +0100

    fix(staging): fixed asgi for websocket connections

--- app/core/asgi.py
+++ app/core/asgi.py
@@ -7,23 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
-from .routing import websocket_urlpatterns
 import os
 
-from channels.auth import AuthMiddlewareStack
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.staging")
+
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 
 from django.core.asgi import get_asgi_application
 
-from .middleware import JWTAuthMiddleware
+
+asgi_appication = get_asgi_application()
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.core.settings.staging")
+def get_application():
 
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": JWTAuthMiddleware(URLRouter(websocket_urlpatterns)),
-    }
-)
+    from .middleware import JWTAuthMiddleware
+    from .routing import websocket_urlpatterns
+
+    return ProtocolTypeRouter(
+        {
+            "http": asgi_appication,
+            "websocket": JWTAuthMiddleware(URLRouter(websocket_urlpatterns)),
+        }
+    )
+
+
+application = get_application()
